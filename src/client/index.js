868ad0eeea63d71e4ceb7785d3cc4af26202eed0
Vivek760 <vivekmenon760@gmail.com>
2020-02-13 23:17:04 +0530

    added React JS code

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,33 @@
+// eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
-  render() {
-    return <h1>Hello, World</h1>;
+class App extends React.Component {
+  state = {
+    count : 0,
+  };
+  increment = () => {
+    const cur = this.state.count;
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: cur + 1,
+    });
   }
-}
+  decrement = () => {
+    const cur = this.state.count;
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: cur - 1,
+    });
+  }
+  render(){
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
 
+    </div>
+  }
+
+}
 render(<App />, document.getElementById('root'));
